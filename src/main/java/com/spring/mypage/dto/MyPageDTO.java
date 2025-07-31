77a79ed3c9ab36c434533b842f0d54e27dee27f8
xorwns <xorwns56@naver.com>
2025-07-31 14:32:55 +0900

    MyPage 파일 세팅

---
+++ src/main/java/com/spring/mypage/dto/MyPageDTO.java
@@ -0,0 +1,8 @@
+package com.spring.mypage.dto;
+
+import lombok.Data;
+
+@Data
+public class MyPageDTO {
+
+}
---
+++ src/main/java/com/spring/mypage/repository/MyPageRepository.java
@@ -0,0 +1,7 @@
+package com.spring.mypage.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MyPageRepository {
+}
